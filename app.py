36a60f5ed65b5a2c0fc1736f49b5f6bc7246fcc4
Muhammad Mubeen Shar Gil <48254408+mubeenshargil@users.noreply.github.com>
2023-02-04 14:32:21 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import json
+from flask import Flask, jsonify, request
+
+
+app=Flask(__name__)
+
+@app.route("/",methods=["GET"])
+def wellcome():
+    return "Wellcome"
+
+@app.route("/",methods=["GET"])
+def main():
+    return "Hello World"
+
+if __name__ =="__main__":
+    app.run(debug=True,host="0.0.0.0",port=8000)
